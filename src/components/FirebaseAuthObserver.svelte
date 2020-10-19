e9d6c772fc74498dae97b16f2ba30e1e87f438fb
Saju Thankappan <sajuthankappan@gmail.com>
2020-10-19 22:01:28 +0530

    Remove unnecessary console logs

--- src/components/FirebaseAuthObserver.svelte
+++ src/components/FirebaseAuthObserver.svelte
@@ -21,15 +21,12 @@
       firebase.auth().onAuthStateChanged(
         async function(user) {
           if (user) {
-            console.log("User is signed in.");
-
             try {
               currentUser.set(user);
               const claims = await isSlickUser(user);
               loading.set(false);
               if (claims) {
                 currentUserClaims.set(claims);
-                console.log("valid slick user");
               } else {
                 goto('/signup');
               }
