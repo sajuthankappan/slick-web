f8f96a79323226b586dd34376b187cb7fff5f072
Saju Thankappan <sajuthankappan@gmail.com>
2021-09-22 18:40:08 +0530

    Fix build errors

--- src/helpers/lighthouse/calc.js
+++ src/helpers/lighthouse/calc.js
@@ -2,8 +2,8 @@
 export function getLighthouseCalculatorUrl(report) {
   const measures = report.webVitals;
   if (report.lighthouseVersion.startsWith('5')) {
-    return `https://googlechrome.github.io/lighthouse/scorecalc/#FCP=${measures.firstContentfulPaint.numericValue}&FMP=${measures.firstMeaningfulPaint.numericValue}&SI=${measures.speedIndex.numericValue}&TTI=${measures.interactive.numericValue}&max-potential-fid=${measures.maxPotentialFid.numericValue}&FCI=${measures.firstCpuIdle?.numericValue}&FMP=${measures.firstMeaningfulPaint.numericValue}&device=${report.configSettings.emulatedFormFactor}&version=5`;
+    return `https://googlechrome.github.io/lighthouse/scorecalc/#FCP=${measures.firstContentfulPaint.numericValue}&FMP=${measures.firstMeaningfulPaint.numericValue}&SI=${measures.speedIndex.numericValue}&TTI=${measures.interactive.numericValue}&max-potential-fid=${measures.maxPotentialFid.numericValue}&FMP=${measures.firstMeaningfulPaint.numericValue}&device=${report.configSettings.emulatedFormFactor}&version=5`;
   }
 
-  return `https://googlechrome.github.io/lighthouse/scorecalc/#FCP=${measures.firstContentfulPaint.numericValue}&SI=${measures.speedIndex.numericValue}&LCP=${measures.largestContentfulPaint.numericValue}&TTI=${measures.interactive.numericValue}&TBT=${measures.totalBlockingTime.numericValue}&CLS=${measures.cumulativeLayoutShift.numericValue}&FCI=${measures.firstCpuIdle?.numericValue}&FMP=${measures.firstMeaningfulPaint.numericValue}&device=${report.configSettings.emulatedFormFactor}`;
+  return `https://googlechrome.github.io/lighthouse/scorecalc/#FCP=${measures.firstContentfulPaint.numericValue}&SI=${measures.speedIndex.numericValue}&LCP=${measures.largestContentfulPaint.numericValue}&TTI=${measures.interactive.numericValue}&TBT=${measures.totalBlockingTime.numericValue}&CLS=${measures.cumulativeLayoutShift.numericValue}&FMP=${measures.firstMeaningfulPaint.numericValue}&device=${report.configSettings.emulatedFormFactor}`;
 }
